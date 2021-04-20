baab6504ea82e108aa90dc972b85556ffafc4178
Dario_Herrera <d.herrerag@uniandes.edu.co>
2021-04-20 15:44:23 -0500

    Tarea 5

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 import{Aprendiz, NivelEducativo} from './aprendiz.js';
+import { Curso } from "./curso.js";
 
-export const ap = new Aprendiz( "Juan Pablo", "Reyes", "avatar.png", 30, NivelEducativo.POSGRADO)
+let cursos = [new Curso('Prácticas escenciales para el agilismo',20,90,true, 2021),
+              new Curso('Ingenieria de software ara la web',10,80,true, 2021),
+              new Curso('Principios de arquitectura y diseño de software',25,95,true, 2021),
+              new Curso('Pruebas automatizadas de software',25,50,false, 2021)];   
 
+export const ap = new Aprendiz( "Juan Pablo", "Reyes", "avatar.png", 30, NivelEducativo.POSGRADO, cursos)
+console.log(ap.cursos);
 let aprendizTable: HTMLElement= document.getElementById("aprendiz")!;
+let estadisticasTable: HTMLElement= document.getElementById("estadisticas")!;
+
 
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 
 function mostrarDatosAprendiz(aprendiz : Aprendiz):void{
     let tbodyAprendiz = document.createElement("tbody");
@@ -12,6 +21,14 @@ function mostrarDatosAprendiz(aprendiz : Aprendiz):void{
     <tr><td>Nombres: </td><td>${aprendiz.nombres}</td></tr>
     <tr><td>Apellidos: </td><td>${aprendiz.apellidos}</td></tr>
     <tr><td>Nivel Educativo: </td><td>${aprendiz.nivelEducativo}</td></tr>
-    <tr><td>Edad: </td><td>${aprendiz.edad}</td></tr>`
+    <tr><td>Edad: </td><td>${aprendiz.edad}</td></tr>`;
     aprendizTable.appendChild(tbodyAprendiz);
+}
+
+function mostrarEstadisticas(aprendiz : Aprendiz):void{
+    let numeroCertificados: number = aprendiz.darCursosCertificados();
+    let trElement:HTMLElement = document.createElement("tr");
+    trElement.innerHTML = `<td><b> Cursos certificados</b></td> <td>${numeroCertificados}</td>`;
+    estadisticasTable.appendChild(trElement);
+
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,22 @@
 import { Aprendiz, NivelEducativo } from './aprendiz.js';
-export var ap = new Aprendiz("Juan Pablo", "Reyes", "avatar.png", 30, NivelEducativo.POSGRADO);
+import { Curso } from "./curso.js";
+var cursos = [new Curso('Prácticas escenciales para el agilismo', 20, 90, true, 2021),
+    new Curso('Ingenieria de software ara la web', 10, 80, true, 2021),
+    new Curso('Principios de arquitectura y diseño de software', 25, 95, true, 2021),
+    new Curso('Pruebas automatizadas de software', 25, 50, false, 2021)];
+export var ap = new Aprendiz("Juan Pablo", "Reyes", "avatar.png", 30, NivelEducativo.POSGRADO, cursos);
 var aprendizTable = document.getElementById("aprendiz");
+var estadisticasTable = document.getElementById("estadisticas");
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 function mostrarDatosAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement("tbody");
-    tbodyAprendiz.innerHTML = "<tr><td colspan=2> <img src=\"./" + aprendiz.avatar + " height=\"100\"></td></tr>\n    <tr><td>Nombres: </td><td>" + aprendiz.nombres + "</td></tr>\n    <tr><td>Apellidos: </td><td>" + aprendiz.apellidos + "</td></tr>\n    <tr><td>Nivel Educativo: </td><td>" + aprendiz.nivelEducativo + "</td></tr>\n    <tr><td>Edad: </td><td>" + aprendiz.edad + "</td></tr>";
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2> <img src=\"./" + aprendiz.avatar + "\" height=\"100\"></td></tr>\n    <tr><td>Nombres: </td><td>" + aprendiz.nombres + "</td></tr>\n    <tr><td>Apellidos: </td><td>" + aprendiz.apellidos + "</td></tr>\n    <tr><td>Nivel Educativo: </td><td>" + aprendiz.nivelEducativo + "</td></tr>\n    <tr><td>Edad: </td><td>" + aprendiz.edad + "</td></tr>";
     aprendizTable.appendChild(tbodyAprendiz);
+}
+function mostrarEstadisticas(aprendiz) {
+    var numeroCertificados = aprendiz.darCursosCertificados();
+    var trElement = document.createElement("tr");
+    trElement.innerHTML = "<td><b> Cursos certificados</b></td> <td>" + numeroCertificados + "</td>";
+    estadisticasTable.appendChild(trElement);
 }
